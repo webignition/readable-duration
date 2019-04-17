4a1fd7412e4bbf90d1cc22a1dc9b5bb6ad5e8453
Jon Cram <webignition@gmail.com>
2019-04-17 13:25:30 +0100

    Revert "Re-implement ReadableDuration, Create Factory (#17)" (#18)
    This reverts commit 1c848b50a5efbe00e3dbd221160edd8028a830d6.

--- tests/DemoTest.php
+++ tests/DemoTest.php
@@ -2,53 +2,39 @@
 
 namespace webignition\Tests\ReadableDuration;
 
-use webignition\ReadableDuration\Factory;
-
-class DemoTest extends \PHPUnit\Framework\TestCase
+class DemoTest extends AbstractReadableDurationTest
 {
-    /**
-     * @var Factory
-     */
-    private $factory;
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        $this->factory = new Factory();
-    }
-
     /**
      * 100000 seconds is 1 day, 3 hours, 46 minutes and 40 seconds
      */
     public function testGetNumberOfYearsMonthsDaysHoursHoursMinutesSeconds()
     {
-        $readableDuration = $this->factory->create(100000);
+        $this->readableDuration->setValueInSeconds(100000);
 
-        $this->assertEquals(1, $readableDuration->getDays());
-        $this->assertEquals(3, $readableDuration->getHours());
-        $this->assertEquals(46, $readableDuration->getMinutes());
-        $this->assertEquals(40, $readableDuration->getSeconds());
+        $this->assertEquals(1, $this->readableDuration->getDays());
+        $this->assertEquals(3, $this->readableDuration->getHours());
+        $this->assertEquals(46, $this->readableDuration->getMinutes());
+        $this->assertEquals(40, $this->readableDuration->getSeconds());
     }
 
     /**
      * 100000 seconds as years, months, days, hours, minute or seconds
      *
-     * Note: these are human-readable convenience representations not exact
+     * Note: these are human-readable convenience representatons not exact
      *
      * 100000 seconds is strictly 1.16 days. As far as convenience is concerned, that's 1 day.
      * 100000 seconds is strictly 27.78 hours. As far as convenience is concerned, that's 28 hours.
      */
     public function testGetInYearsMonthsDaysHoursMinutesOrSeconds()
     {
-        $readableDuration = $this->factory->create(100000);
+        $this->readableDuration->setValueInSeconds(100000);
 
-        $this->assertEquals(0, $readableDuration->getInYears());
-        $this->assertEquals(0, $readableDuration->getInMonths());
-        $this->assertEquals(1, $readableDuration->getInDays());
-        $this->assertEquals(28, $readableDuration->getInHours());
-        $this->assertEquals(1667, $readableDuration->getInMinutes());
-        $this->assertEquals(100000, $readableDuration->getInSeconds());
+        $this->assertEquals(0, $this->readableDuration->getInYears());
+        $this->assertEquals(0, $this->readableDuration->getInMonths());
+        $this->assertEquals(1, $this->readableDuration->getInDays());
+        $this->assertEquals(28, $this->readableDuration->getInHours());
+        $this->assertEquals(1667, $this->readableDuration->getInMinutes());
+        $this->assertEquals(100000, $this->readableDuration->getInSeconds());
     }
 
     /**
@@ -61,59 +47,50 @@ class DemoTest extends \PHPUnit\Framework\TestCase
      */
     public function testGetAsTheMostAppropriateHumanValue()
     {
-        $readableDuration = $this->factory->create(100000);
+        $this->readableDuration->setValueInSeconds(100000);
 
         /**
          * 100000 seconds, as a single time unit is 1 day
          */
-        $this->assertEquals(
-            [
-                [
-                    'unit' => 'day',
-                    'value' => 1
-                ]
-            ],
-            $this->factory->getInMostAppropriateUnits($readableDuration)
-        );
+        $this->assertEquals(array(
+            array(
+                'unit' => 'day',
+                'value' => 1
+            )
+        ), $this->readableDuration->getInMostAppropriateUnits());
 
 
         /**
          * 100000 seconds, as two time units is 1 day 4 hours
          */
-        $this->assertEquals(
-            [
-                [
-                    'unit' => 'day',
-                    'value' => 1
-                ],
-                [
-                    'unit' => 'hour',
-                    'value' => 4
-                ]
-            ],
-            $this->factory->getInMostAppropriateUnits($readableDuration, 2)
-        );
+        $this->assertEquals(array(
+            array(
+                'unit' => 'day',
+                'value' => 1
+            ),
+            array(
+                'unit' => 'hour',
+                'value' => 4
+            )
+        ), $this->readableDuration->getInMostAppropriateUnits(2));
 
 
         /**
          * 100000 seconds, as three time units is 1 day 3 hours 47 minutes
          */
-        $this->assertEquals(
-            [
-                [
-                    'unit' => 'day',
-                    'value' => 1
-                ],
-                [
-                    'unit' => 'hour',
-                    'value' => 3
-                ],
-                [
-                    'unit' => 'minute',
-                    'value' => 47
-                ]
-            ],
-            $this->factory->getInMostAppropriateUnits($readableDuration, 3)
-        );
+        $this->assertEquals(array(
+            array(
+                'unit' => 'day',
+                'value' => 1
+            ),
+            array(
+                'unit' => 'hour',
+                'value' => 3
+            ),
+            array(
+                'unit' => 'minute',
+                'value' => 47
+            )
+        ), $this->readableDuration->getInMostAppropriateUnits(3));
     }
 }
