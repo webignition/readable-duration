f7789044c9d31daf07f91065d0785044330d3bf2
Jon Cram <jon@webignition.net>
2019-04-17 09:51:33 +0100

    Fix getting duration in most appropriate units

--- src/ReadableDuration.php
+++ src/ReadableDuration.php
@@ -7,9 +7,13 @@ class ReadableDuration
     const SECONDS_PER_MINUTE = 60;
     const MINUTES_PER_HOUR = 60;
     const HOURS_PER_DAY = 24;
-    const DAYS_PER_MONTH = 30.44; // approximate!
+    const DAYS_PER_MONTH = 30; // approximate!
     const MONTHS_PER_YEAR = 12;
-    const DAYS_PER_YEAR = 365.25; // approximate!
+    const DAYS_PER_YEAR = 365; // approximate!
+
+    const SECONDS_PER_DAY = self::SECONDS_PER_MINUTE * self::MINUTES_PER_HOUR * self::HOURS_PER_DAY;
+    const SECONDS_PER_HOUR = self::SECONDS_PER_MINUTE * self::MINUTES_PER_HOUR;
+    const SECONDS_PER_YEAR = self::SECONDS_PER_DAY * self::DAYS_PER_YEAR;
 
     const MAX_APPROPRIATE_UNITS_PRECISION = 6;
 
