fa73269da33758425014a6fff6f915cd35c00f4d
Jon Cram <jon@webignition.net>
2019-04-17 11:10:50 +0100

    Tidy up

--- src/ReadableDuration.php
+++ src/ReadableDuration.php
@@ -13,8 +13,8 @@ class ReadableDuration
 
     const SECONDS_PER_DAY = self::SECONDS_PER_MINUTE * self::MINUTES_PER_HOUR * self::HOURS_PER_DAY;
     const SECONDS_PER_HOUR = self::SECONDS_PER_MINUTE * self::MINUTES_PER_HOUR;
-    const SECONDS_PER_YEAR = self::SECONDS_PER_DAY * self::DAYS_PER_YEAR;
     const SECONDS_PER_MONTH = self::SECONDS_PER_DAY * self::DAYS_PER_MONTH;
+    const SECONDS_PER_YEAR = self::SECONDS_PER_DAY * self::DAYS_PER_YEAR;
 
     const MAX_APPROPRIATE_UNITS_PRECISION = 6;
 
